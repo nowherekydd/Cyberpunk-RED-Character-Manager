64b2cf24c585841ca5c4208c03979a7b59848368
nowherekydd <james.burnham.2001@gmail.com>
2024-07-17 16:18:25 -0400

    Saving
    Saving just in case the computer hard crashes again from smth unrelated

--- starsheet.py
+++ starsheet.py
@@ -37,20 +37,28 @@ deathSave = starscore.bodyScore
 
 # Humanity [includes current EMP because based on that]
 # Max HUM Penalty
-cyberCount = 5
+cyberCount = 4
 maxHUMPenalty = cyberCount * 2
 
 maxHUM = (10 * starscore.maxempScore) - maxHUMPenalty
-humLoss = 49
+humLoss = 37 # Temp mathn't until I get around to it.
 humScore = maxHUM - humLoss
 empScore = math.floor(humScore / 10)
+
+# Charsheet extras.
+
+charHeader = f"{charName} aka " + txtfrm.bold(f"{charHandle}")
+
+charhCenter = charHeader.center(85)
 
 # Charsheet format here.
 
 def charDisplay():
     asciiart.charsheetheader()
-    print(txtfrm.bold("Test"))
-    print("Test successful.")
+    asciiart.charsheetdiv()
+    print(charhCenter)
+    asciiart.charsheetdiv()
+    txtfrm.linebreak()
     
 # Prints out charsheet on run for debug/testing.
 
