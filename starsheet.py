95c056961e2e226536133697af759c542f8f8bc2
nowherekydd <james.burnham.2001@gmail.com>
2024-07-18 17:58:36 -0400

    Score > Sheet
    Consolidated starscore to starsheet

--- starsheet.py
+++ starsheet.py
@@ -13,10 +13,60 @@
 
 import math
 import os
-import starscore
 import starskill
 import asciiart
 import txtfrm
+
+#################################################################################################
+# Here is where you would put in your character's information. I haven't tested this with any   #
+# character other than Starboy, so if something breaks... gods help us all.                     #
+#################################################################################################
+
+################# BASIC INFORMATION ####################
+
+charName = "Astarion Ancunin"
+charHandle = "Starboy"
+charRole = "Rockerboy"
+roleAbility = "Charismatic Impact"
+
+########################################################
+
+################# CHARACTER SCORES #####################
+
+ntlScore = 5
+refScore = 7
+dexScore = 6
+techScore = 6
+coolScore = 8
+willScore = 8
+moveScore = 5
+bodyScore = 5
+maxempScore = 7
+maxluckScore = 5
+
+########################################################
+
+################# DERIVED SCORES #######################
+## HP = 10 + (5 * ((BODY+WILL)/2, round up))
+maxHP = 10 + (5 * math.ceil((bodyScore + willScore)/2))
+sevWound = math.ceil(maxHP / 2)
+deathSave = bodyScore
+
+# Max Luck
+luckScore = maxluckScore - 0
+
+# HUM = EMP * 10 - max is reduced by 2 per cyber. Some values are manual at this time.
+cyberCount = 4
+maxHUMPenalty = cyberCount * 2
+maxHUM = (10 * maxempScore) - maxHUMPenalty
+humLoss = 37 # Temp mathn't until I get around to it.
+humScore = maxHUM - humLoss
+empScore = math.floor(humScore / 10)
+
+#################################################################################################
+# Formatting beyond this point. It's not rec'd that you edit anything pas there. If you do, you #
+# do so at your own risk.                                                                       #
+#################################################################################################
 
 # Clears console
 clear = lambda: print("\033c", end="", flush=True)
@@ -39,55 +89,26 @@ def center_text(text, width):
     return centered_text
 
 
-width = get_terminal_width()
+width = get_terminal_width() # Centering operator
 
-nbsp = "\u00A0"
+nbsp = "\u00A0" # Spacer
 
-# Basic char info.
-
-charName = "Astarion Ancunin"
-charHandle = "Starboy"
-charRole = "Rockerboy"
-roleAbility = "Charismatic Impact"
-
-#####################################
-
-# Define derived scores.
-## HP = 10 + (5 * ((BODY+WILL)/2, round up))
-## HUM = EMP * 10 - max is reduced by 2 per cyber
-
-# HP
-maxHP = 10 + (5 * math.ceil((starscore.bodyScore + starscore.willScore)/2))
-sevWound = math.ceil(maxHP / 2)
-deathSave = starscore.bodyScore
-
-# Humanity [includes current EMP because based on that]
-# Max HUM Penalty
-cyberCount = 4
-maxHUMPenalty = cyberCount * 2
-
-maxHUM = (10 * starscore.maxempScore) - maxHUMPenalty
-humLoss = 37 # Temp mathn't until I get around to it.
-humScore = maxHUM - humLoss
-empScore = math.floor(humScore / 10)
-
-# Charsheet funsies.
+# Defines score display so that charScores isn't ball-bustingly long
 
 charSPACE = txtfrm.rollerheader(center_text(nbsp, 42))
 charEND = txtfrm.rollerheader(center_text(nbsp, 43))
-charINT = txtfrm.rollermenu(txtfrm.bold(center_text(f"{starscore.ntlScore}", 3))) + txtfrm.rollermenu(center_text("INT //", 6))
-charREF = txtfrm.rollermenu(txtfrm.bold(center_text(f"{starscore.refScore}", 3))) + txtfrm.rollermenu(center_text("REF //", 6))
-charDEX = txtfrm.rollermenu(txtfrm.bold(center_text(f"{starscore.dexScore}", 3))) + txtfrm.rollermenu(center_text("DEX //", 6))
-charTECH = txtfrm.rollermenu(txtfrm.bold(center_text(f"{starscore.techScore}", 3))) + txtfrm.rollermenu(center_text("TECH //", 7))
-charCOOL = txtfrm.rollermenu(txtfrm.bold(center_text(f"{starscore.coolScore}", 3))) + txtfrm.rollermenu(center_text("COOL //", 7))
-charWILL = txtfrm.rollermenu(txtfrm.bold(center_text(f"{starscore.willScore}", 3))) + txtfrm.rollermenu(center_text("WILL //", 7))
-charMOVE = txtfrm.rollermenu(txtfrm.bold(center_text(f"{starscore.moveScore}", 3))) + txtfrm.rollermenu(center_text("MOVE //", 7))
-charBODY = txtfrm.rollermenu(txtfrm.bold(center_text(f"{starscore.bodyScore}", 3))) + txtfrm.rollermenu(center_text("BODY //", 7))
-charEMP = txtfrm.rollermenu(txtfrm.bold(center_text(f"{starscore.empScore}", 3))) + txtfrm.rollermenu(center_text(f"/ {starscore.maxempScore}", 4)) + txtfrm.rollermenu(center_text("EMP //", 7))
-charLUCK = txtfrm.rollermenu(txtfrm.bold(center_text(f"{starscore.luckScore}", 3))) + txtfrm.rollermenu(center_text(f"/ {starscore.maxluckScore}", 4)) + txtfrm.rollermenu(center_text("LUCK", 7))
+charINT = txtfrm.rollermenu(txtfrm.bold(center_text(f"{ntlScore}", 3))) + txtfrm.rollermenu(center_text("INT //", 6))
+charREF = txtfrm.rollermenu(txtfrm.bold(center_text(f"{refScore}", 3))) + txtfrm.rollermenu(center_text("REF //", 6))
+charDEX = txtfrm.rollermenu(txtfrm.bold(center_text(f"{dexScore}", 3))) + txtfrm.rollermenu(center_text("DEX //", 6))
+charTECH = txtfrm.rollermenu(txtfrm.bold(center_text(f"{techScore}", 3))) + txtfrm.rollermenu(center_text("TECH //", 7))
+charCOOL = txtfrm.rollermenu(txtfrm.bold(center_text(f"{coolScore}", 3))) + txtfrm.rollermenu(center_text("COOL //", 7))
+charWILL = txtfrm.rollermenu(txtfrm.bold(center_text(f"{willScore}", 3))) + txtfrm.rollermenu(center_text("WILL //", 7))
+charMOVE = txtfrm.rollermenu(txtfrm.bold(center_text(f"{moveScore}", 3))) + txtfrm.rollermenu(center_text("MOVE //", 7))
+charBODY = txtfrm.rollermenu(txtfrm.bold(center_text(f"{bodyScore}", 3))) + txtfrm.rollermenu(center_text("BODY //", 7))
+charEMP = txtfrm.rollermenu(txtfrm.bold(center_text(f"{empScore}", 3))) + txtfrm.rollermenu(center_text(f"/ {maxempScore}", 4)) + txtfrm.rollermenu(center_text("EMP //", 7))
+charLUCK = txtfrm.rollermenu(txtfrm.bold(center_text(f"{luckScore}", 3))) + txtfrm.rollermenu(center_text(f"/ {maxluckScore}", 4)) + txtfrm.rollermenu(center_text("LUCK", 7))
 
 def charHead():
-    width = get_terminal_width()
     print(txtfrm.rollermenu(center_text(f"{charName} aka", width)))
     print(txtfrm.bold(txtfrm.rollermenu(center_text(charHandle, width))))
 
@@ -108,4 +129,4 @@ def charDisplay():
 
     
 # Prints out charsheet on run for debug/testing. Comment out when not in use.
-# charDisplay()
+charDisplay()
